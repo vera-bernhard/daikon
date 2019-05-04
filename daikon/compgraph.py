ffac2a3a53ad69d5994fe2e7387bca22a21e9f42
vera-bernhard <38719534+vera-bernhard@users.noreply.github.com>
2019-05-04 21:24:25 +0200

    Dropout on Encoder & Decoder added

--- daikon/compgraph.py
+++ daikon/compgraph.py
@@ -40,16 +40,22 @@ def define_computation_graph(source_vocab_size: int, target_vocab_size: int, bat
 
     with tf.variable_scope("Encoder"):
         encoder_cell = tf.contrib.rnn.LSTMCell(C.HIDDEN_SIZE)
+        # apply dropout, 0.8 = keep probability
+        dropout = tf.nn.dropout(encoder_cell, 0.8)
         initial_state = encoder_cell.zero_state(batch_size, tf.float32)
 
-        encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(encoder_cell,
+        # this is now applied on dropout instead of encoder_cell
+        encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(dropout,
                                                                  encoder_inputs_embedded,
                                                                  initial_state=initial_state,
                                                                  dtype=tf.float32)
 
     with tf.variable_scope("Decoder"):
         decoder_cell = tf.contrib.rnn.LSTMCell(C.HIDDEN_SIZE)
-        decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn(decoder_cell,
+        # apply dropout, 0.8 = keep probability
+        dropout = tf.nn.dropout(decoder_cell, 0.8) 
+        # this is now applied on dropout instead of encoder_cell
+        decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn(dropout,
                                                                  decoder_inputs_embedded,
                                                                  initial_state=encoder_final_state,
                                                                  dtype=tf.float32)
